806069df1a2834d8e75fa6a94fc4ef56d7db10e0
Waghmare, Amol <Amol_Waghmare@intuit.com>
2017-04-01 21:54:11 -0700

    Adding readme file

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Waghmare, Amol on 01/04/17.
+//  Copyright © 2017 Waghmare, Amol. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
